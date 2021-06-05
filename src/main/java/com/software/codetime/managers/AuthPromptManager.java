8ef553929dc2e0a97c4f93b9e84c5bc621192e7b
Xavier Luiz <xavluiz@gmail.com>
2021-06-04 22:40:55 -0700

    added auth prompt manager

---
+++ src/main/java/com/software/codetime/managers/AuthPromptManager.java
@@ -0,0 +1,39 @@
+package com.software.codetime.managers;
+
+import com.intellij.openapi.util.IconLoader;
+import org.apache.commons.lang.StringUtils;
+import swdc.java.ops.snowplow.events.UIInteractionType;
+
+import javax.swing.*;
+
+public class AuthPromptManager {
+
+    public static void initiateSwitchAccountFlow() {
+        initiateAuthFlow("Switch account", "Switch to a different account?");
+    }
+
+    public static void initiateSignupFlow() {
+        initiateAuthFlow("Sign up", "Sign up using...");
+    }
+
+    public static void initiateLoginFlow() {
+        initiateAuthFlow("Log in", "Log in using...");
+    }
+
+    private static void initiateAuthFlow(String title, String message) {
+        String[] options = new String[]{ "Google", "GitHub", "Email" };
+        Icon icon = IconLoader.getIcon("/assets/paw-grey.png");
+        String input = (String) JOptionPane.showInputDialog(
+                null,
+                message,
+                title,
+                JOptionPane.OK_CANCEL_OPTION,
+                icon,
+                options, // Array of choices
+                options[0]); // Initial choice
+        if (StringUtils.isNotBlank(input)) {
+            UserSessionManager.launchLogin(input.toLowerCase(), UIInteractionType.click, true);
+        }
+
+    }
+}
