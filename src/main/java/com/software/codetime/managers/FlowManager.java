bc37652b4a274700847fdb37ff12e7c348cffbe7
Xavier Luiz <35306917+xavluiz@users.noreply.github.com>
2022-02-07 10:38:34 -0800

    use app auth endpoints and simplify flow mgr

--- src/main/java/com/software/codetime/managers/FlowManager.java
+++ src/main/java/com/software/codetime/managers/FlowManager.java
@@ -2,11 +2,8 @@ package com.software.codetime.managers;
 
 import com.intellij.openapi.application.ApplicationManager;
 import com.software.codetime.toolwindows.codetime.CodeTimeWindowFactory;
-import swdc.java.ops.event.SlackStateChangeModel;
 import swdc.java.ops.http.FlowModeClient;
 import swdc.java.ops.manager.*;
-
-import javax.swing.*;
 
 public class FlowManager {
 
@@ -25,7 +22,6 @@ public class FlowManager {
     }
 
     public static void enterFlowMode(boolean automated) {
-
         boolean isRegistered = AccountManager.checkRegistration(false, null);
         if (!automated && !isRegistered) {
             // show the flow mode prompt
@@ -33,33 +29,7 @@ public class FlowManager {
             return;
         }
 
-        boolean intellij_CtskipSlackConnect = FileUtilManager.getBooleanItem("intellij_CtskipSlackConnect");
-        boolean workspaces = SlackManager.hasSlackWorkspaces();
-        boolean isInFlow = FileUtilManager.getFlowChangeState();
-        if (!automated && !workspaces && !intellij_CtskipSlackConnect && !isInFlow) {
-            String msg = "Connect a Slack workspace to pause\nnotifications and update your status?";
-
-            Object[] options = {"Connect", "Skip"};
-            Icon icon = UtilManager.getResourceIcon("app-icon-blue.png", FlowManager.class.getClassLoader());
-
-            SwingUtilities.invokeLater(() -> {
-                int choice = JOptionPane.showOptionDialog(
-                        null, msg, "Slack connect", JOptionPane.OK_OPTION,
-                        JOptionPane.QUESTION_MESSAGE, icon, options, options[0]);
-
-                if (choice == 0) {
-                    SlackStateChangeModel changeModel = new SlackStateChangeModel();
-                    SlackManager.connectSlackWorkspace(() -> {
-                        CodeTimeWindowFactory.refresh(true);
-                    });
-                } else {
-                    FileUtilManager.setBooleanItem("intellij_CtskipSlackConnect", true);
-                    FlowManager.enterFlowMode(automated);
-                    FileUtilManager.updateFlowChangeState(true);
-                }
-            });
-            return;
-        } else if (!isInFlow) {
+        if (!FileUtilManager.getFlowChangeState()) {
             // go ahead and make the api call to enter flow mode
             FlowModeClient.enterFlowMode(automated);
             FileUtilManager.updateFlowChangeState(true);
